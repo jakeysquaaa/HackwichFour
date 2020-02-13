bdfd6836ee22e851a979d49fd1d0c7a7671dfc26
CM Student <cmstudent@uhwo-acm-11.local>
2020-02-13 10:37:47 -1000

    completed part 7

--- HackwichFour/ViewController.swift
+++ HackwichFour/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        //set the text to the firstLabel to "About"
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
+        //we want to set first label to about
+        self.firstLabel.text = "about"
+        
+    
     }
 
 
